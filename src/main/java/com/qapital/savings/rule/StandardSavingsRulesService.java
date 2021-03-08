55ca94ec3dfab64d0c5d804758edb76c208e6d41
Olof Larsson <oloflarsson1989@gmail.com>
2021-03-08 12:19:46 +0100

    WIP naive initial executeRule without unit tests

--- src/main/java/com/qapital/savings/rule/StandardSavingsRulesService.java
+++ src/main/java/com/qapital/savings/rule/StandardSavingsRulesService.java
@@ -1,12 +1,15 @@
 package com.qapital.savings.rule;
 
+import com.qapital.bankdata.transaction.Transaction;
 import com.qapital.bankdata.transaction.TransactionsService;
 import com.qapital.savings.event.SavingsEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class StandardSavingsRulesService implements SavingsRulesService {
@@ -32,7 +35,43 @@ public class StandardSavingsRulesService implements SavingsRulesService {
 
     @Override
     public List<SavingsEvent> executeRule(SavingsRule savingsRule) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return transactionsService.latestTransactionsForUser(savingsRule.getUserId()).stream()
+                .filter(transaction -> isApplicable(savingsRule, transaction))
+                .map(transaction -> executeRule(savingsRule, transaction))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
     }
 
+    private static boolean isApplicable(SavingsRule savingsRule, Transaction transaction) {
+        return Objects.equals(savingsRule.getUserId(), transaction.getUserId()) &&
+                savingsRule.isActive() &&
+                transaction.getAmount() < 0;
+    }
+
+    private static List<SavingsEvent> executeRule(SavingsRule savingsRule, Transaction transaction) {
+        double savingsAmountTotal = getSavingsAmountTotal(savingsRule, transaction);
+        double amount = savingsAmountTotal / savingsRule.getSavingsGoalIds().size();
+
+        return savingsRule.getSavingsGoalIds().stream()
+                .map(savingsGoalId -> SavingsEvent.ofRuleApplication(savingsRule, amount, savingsGoalId))
+                .collect(Collectors.toList());
+    }
+
+    private static double getSavingsAmountTotal(SavingsRule savingsRule, Transaction transaction) {
+        SavingsRule.RuleType ruleType = savingsRule.getRuleType();
+
+        if (ruleType == SavingsRule.RuleType.roundup) {
+            return -transaction.getAmount() % savingsRule.getAmount();
+        }
+
+        if (ruleType == SavingsRule.RuleType.guiltypleasure) {
+            if (Objects.equals(transaction.getDescription(), savingsRule.getPlaceDescription())) {
+                return savingsRule.getAmount();
+            } else {
+                return 0;
+            }
+        }
+
+        throw new UnsupportedOperationException("Not Implemented: " + ruleType);
+    }
 }
